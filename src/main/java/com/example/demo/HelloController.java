08518a0a885ae58173e7d81ca499a1c8bdefa645
Matthew <merikso@g.clemson.edu>
2021-09-17 11:06:04 -0400

    added buildspec.yml and plugin for exploding war

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello") // this can be reached at localhost:8080/hello
+@RestController
+public class HelloController {
+	
+	@GetMapping
+	public String hello() {
+		
+		return "What's up?!";
+		
+	}
+}
